fc2be78e74c2ed9f02671e779ecdebf126cb8677
yansi <yansi@yansi-As>
2018-01-26 17:57:13 -0300

    new commit

--- src/main/java/com/yansi/sCadastro/controller/Login.java
+++ src/main/java/com/yansi/sCadastro/controller/Login.java
@@ -2,17 +2,12 @@
 package com.yansi.sCadastro.controller;
 
 import com.yansi.sCadastro.Modelo.ModUsuario;
-import java.util.List;
 import javax.swing.JOptionPane;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriInfo;
-import org.glassfish.jersey.message.internal.MediaTypes;
 
 @Path("acessologin")
 public class Login {
