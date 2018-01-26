f41235af35821ec8f5fc27290491dad1d081c067
yansi <yansi@yansi-As>
2018-01-26 16:01:46 -0300

    Implementação dia 26/01/2018

--- src/main/java/com/yansi/sCadastro/controller/Login.java
+++ src/main/java/com/yansi/sCadastro/controller/Login.java
@@ -1,19 +1,35 @@
 
 package com.yansi.sCadastro.controller;
 
-import javax.ws.rs.GET;
+import com.yansi.sCadastro.Modelo.ModUsuario;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+import org.glassfish.jersey.message.internal.MediaTypes;
 
-@Path("/acessologin")
+@Path("acessologin")
 public class Login {
     
-    @GET
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String validaAcesso () {
-        String status = null;
+    public ModUsuario getValidaAcesso (ModUsuario modUsuario) {
+        String status = "Olá Mundo 1";
         
-        return status;
+        JOptionPane.showMessageDialog(null, modUsuario.getNome());
+        JOptionPane.showMessageDialog(null, modUsuario.getSenha());
+        JOptionPane.showMessageDialog(null, modUsuario.getUsuario());
+        JOptionPane.showMessageDialog(null, modUsuario.getIdade());
+        JOptionPane.showMessageDialog(null, modUsuario.getId());
+        System.out.println(status);
+        
+        return modUsuario;
     }
 }
